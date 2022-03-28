13fb730bec677faf1f71dc6588d1a768470cf467
Hélder Gonçalves <helderpimentaxc@gmail.com>
2022-03-28 10:25:08 +0100

    Update

--- app/src/main/java/com/bitdev/bussinessplan/MainActivity.kt
+++ app/src/main/java/com/bitdev/bussinessplan/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.bitdev.bussinessplan
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firebase = Firebase.initialize(this)
+
+        Log.e("firebase", firebase.toString())
     }
 }
